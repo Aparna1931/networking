0a2dde22aa7c54a1160d9ce1d19cf3f0ef9fceff
Aparna1931 <aparnamohan835@gmail.com>
2025-07-06 19:14:01 +0100

    update

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "networking-ca1-20065124"
     key            = "terraform/terraform.tfstate"
-    region         = "eu-central-1"
+    region         = "eu-north-1"
     encrypt        = true
   }
 }
