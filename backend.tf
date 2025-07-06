9a62cf6c98824036ff029fb41de7548b881c9fa9
Aparna1931 <aparnamohan835@gmail.com>
2025-07-06 18:50:07 +0100

    update

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "vardhanavprojectx"
-    key            = "ec2-docker/terraform.tfstate"
+    bucket         = "networking-ca1-20065124"
+    key            = "terraform/terraform.tfstate"
     region         = "eu-central-1"
     encrypt        = true
   }
